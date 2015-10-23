2f5b8ce0ce0f2910349fde9f68887fa168648fda
Simon Kokkendorff <sik@dmi.dk>
2015-10-23 11:02:35 +0200

    initial code import (some code from laspy)

---
+++ lasviewer.py
@@ -0,0 +1,335 @@
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtOpenGL import *
+import numpy as np
+import traceback
+import threading
+import sys
+import os
+import laspy.file as lasf
+import qt_glviewer
+
+ABOUT = "A pointcloud viewer based on laspy"
+
+CLS_MAP = {1: (0.9, 0.9, .9), 6: (0.8, 0, 0), 9: (0, 0, 0.9), 2: (0.6, 0.5, 0), 3: (
+    0, 0.8, 0), 4: (0, 0.6, 0), 5: (0.1, 0.9, 0), 17: (0, 0.35, 0.3), 18: (0.1, 0.5, 0.5)}
+
+COLOR_LIST = ((0.9, 0, 0), (0, 0.9, 0), (0, 0, 0.9),
+              (0.7, 0.8, 0), (0, 0.8, 0.7))
+
+
+# Some colormap methods
+def class_to_color(cls, cls_map):
+    colors = np.ones((cls.shape[0], 3), dtype=np.float32) * 0.5
+    for c in cls_map:
+        colors[cls == c] = cls_map[c]
+    return colors
+
+
+def discrete_dimension_to_color(all_vals, color_list):
+    colors = np.zeros((all_vals.shape[0], 3), dtype=np.float32)
+    vals = np.unique(all_vals)
+    for i, val in enumerate(vals):
+        M = (all_vals == val)
+        colors[M] = color_list[i % len(color_list)]
+    return colors
+
+
+def linear_colormap(all_vals, color_low, color_high):
+    c1 = np.ones((all_vals.shape[0], 3), dtype=np.float32) * color_low
+    c2 = np.ones((all_vals.shape[0], 3), dtype=np.float32) * color_high
+    m1 = np.percentile(all_vals, 5)
+    m2 = np.percentile(all_vals, 95)
+    dv = ((all_vals - m1) / (m2 - m1)).reshape((all_vals.shape[0], 1))
+    dv[dv < 0] = 0
+    dv[dv > 1] = 1
+    colors = c1 + c2 * dv
+    return colors
+
+
+class RedirectOutput(object):
+
+    def __init__(self, win, signal):
+        self.win = win
+        self.signal = signal
+        self.buffer = ""
+
+    def write(self, text):
+        self.buffer += text
+        if self.buffer[-1] == "\n":
+            self.flush()
+
+    def flush(self):
+        if len(self.buffer) == 0:
+            return
+        if self.buffer[-1] == "\n":
+            self.win.emit(self.signal, self.buffer[:-1])
+        else:
+            self.win.emit(self.signal, self.buffer)
+        self.buffer = ""
+
+
+class TextViewer(QDialog):
+    """Class to display text output"""
+
+    def __init__(self, parent):
+        QDialog.__init__(self, parent)
+        self.setWindowTitle("Log")
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.txt_field = QTextEdit(self)
+        self.txt_field.setCurrentFont(QFont("Courier", 9))
+        self.txt_field.setSizePolicy(
+            QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.txt_field.setReadOnly(True)
+        self.txt_field.setMinimumSize(600, 200)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.txt_field)
+
+    def log(self, text, color):
+        self.txt_field.setTextColor(QColor(color))
+        self.txt_field.append(text)
+        self.txt_field.ensureCursorVisible()
+
+
+menu_model = (
+    {"name": "&File",
+     "items": (("Open", "onOpenFile"),
+               ("About", "onAbout"),
+               ("Exit", "onClose"))
+     },
+    {"name": "&Color",
+     "items": (("By class", "colorByClass"),
+               ("By z", "colorByZ"),
+               ("By return", "colorByRetNum"),
+               ("By source id", "colorByPid"),
+               ("By rgb", "colorByRGB"),
+               ("By intensity", "colorByIntensity"))
+     },
+    {"name": "&Display",
+     "items": (("Increase point size", "increasePointSize"),
+               ("Decrease point size", "decreasePointSize"),
+               ("Reset", "resetView"))}
+)
+
+
+class LasViewer(QtGui.QMainWindow):
+    """Container for PointCloudViewerWidget"""
+
+    def __init__(self, fname=None):
+        QtGui.QMainWindow.__init__(self)
+        self.setWindowTitle("LasViewer")
+        self.viewer = qt_glviewer.PointcloudViewerWidget(self)
+        self.setCentralWidget(self.viewer)
+        menubar = self.menuBar()
+        for menu_def in menu_model:
+            menu = menubar.addMenu(menu_def["name"])
+            for item, handler in menu_def["items"]:
+                action = QAction(item, self)
+                menu.addAction(action)
+                if handler:
+                    action.triggered.connect(getattr(self, handler))
+
+        self.dir = "/"
+        self.logWindow = TextViewer(self)
+
+        # threading stuff
+        self.background_task_signal = QtCore.SIGNAL("__my_backround_task")
+        self.log_stdout_signal = QtCore.SIGNAL("__stdout_signal")
+        self.log_stderr_signal = QtCore.SIGNAL("__stderr_signal")
+        QtCore.QObject.connect(
+            self, self.background_task_signal, self.finishBackgroundTask)
+        QtCore.QObject.connect(self, self.log_stdout_signal, self.logStdout)
+        QtCore.QObject.connect(self, self.log_stderr_signal, self.logStderr)
+        self.filename = None
+        self.display_dimension = "raw_classification"
+        self.lasf_object = None
+        self.err_msg = None
+        # redirect textual output
+        if "debug" not in sys.argv:
+            sys.stdout = RedirectOutput(self, self.log_stdout_signal)
+            sys.stderr = RedirectOutput(self, self.log_stderr_signal)
+        self.show()
+        if fname is not None:
+            self.openFile(fname)
+        else:
+            QMessageBox.information(
+                self, "Movement", "Use first and second mouse button as well as 'asdw' to move around.")
+
+    # Menu event handlers here
+    def onClose(self):
+        self.close()
+
+    def onAbout(self):
+        msg = ABOUT
+        QMessageBox.about(self, "About", msg)
+
+    def onOpenFile(self):
+        my_file = unicode(QFileDialog.getOpenFileName(
+            self, "Select a vector-data input file", self.dir))
+        if len(my_file) > 0:
+            self.openFile(my_file)
+
+    def colorByClass(self):
+        self.display_dimension = "raw_classification"
+        self.onChangeColorMode()
+
+    def colorByZ(self):
+        self.display_dimension = "z"
+        self.onChangeColorMode()
+
+    def colorByRGB(self):
+        self.display_dimension = "rgb"
+        self.onChangeColorMode()
+
+    def colorByPid(self):
+        self.display_dimension = "pt_src_id"
+        self.onChangeColorMode()
+
+    def colorByRetNum(self):
+        self.display_dimension = "return_num"
+        self.onChangeColorMode()
+
+    def colorByIntensity(self):
+        self.display_dimension = "intensity"
+        self.onChangeColorMode()
+
+    def increasePointSize(self):
+        self.viewer.increase_point_size()
+
+    def decreasePointSize(self):
+        self.viewer.decrease_point_size()
+
+    def resetView(self):
+        self.viewer.reset_all()
+
+    # Other methods
+    def onChangeColorMode(self):
+        if self.lasf_object is not None:
+            self.runInBackground(self._setColorsInBackground)
+
+    def openFile(self, my_file):
+        self.dir = os.path.dirname(my_file)
+        self.log("Opening " + my_file + "...")
+        self.filename = my_file
+        if self.lasf_object is not None:  # hmm check destructor
+            self.lasf_object.close()
+        self.runInBackground(self._loadInBackground)
+
+    def runInBackground(self, run_method):
+        """
+        Start a background task.
+        finishBackgroundTask should be triggered by an event when done.
+        """
+        self.setEnabled(False)
+        self.err_msg = None  # Nothing bad - yet!
+        thread = threading.Thread(target=run_method)
+        # probably exceptions in the run method should be handled
+        # there in order to avoid a freeze...
+        # Avoud calling GUI methods in thread!
+        thread.start()
+
+    def finishBackgroundTask(self):
+        # This is called from an emmitted event -
+        # the last execution from the run method...
+        self.setEnabled(True)
+        if self.err_msg is not None:
+            # Something went wrong!
+            raise Exception(self.err_msg)
+        else:
+            # Everything should have been transfered to the viewer
+            # and we're good to go
+            self.statusBar().showMessage("Source: %s, display dimension: %s" %
+                                         (self.filename, self.display_dimension))
+            self.viewer.update_view()
+
+    def _setColorsInBackground(self):
+        self.setColors()
+        self.emit(self.background_task_signal)
+
+    def setColors(self):
+        """
+        This can happen in a background thread,
+        so beware not to call any GUI methods.
+        """
+        dim = self.display_dimension
+
+        try:
+            if dim == "rgb":
+                self.log("Getting rgb")
+                colors = np.array([getattr(self.lasf_object, color) for color
+                                   in ("red", "green", "blue")]).T.astype(np.float32)
+                _max = np.max(colors)
+                _min = np.min(colors)
+                diff = _max - _min
+                colors -= _min
+                colors /= diff
+            else:
+                self.log("Getting dimension " + dim)
+                data = getattr(self.lasf_object, dim)
+                self.log("Generating colors...")
+                if dim == "intensity":  # normalise
+                    data = data.astype(np.float32)
+                    data /= data.max()
+                if dim == "raw_classification":
+                    colors = class_to_color(data, CLS_MAP)
+                elif data.dtype == np.float32 or data.dtype == np.float64:
+                    colors = linear_colormap(
+                        data, (0.1, 0.1, 0.1), (0.9, 0.9, 0.9))
+                else:
+                    colors = discrete_dimension_to_color(data, COLOR_LIST)
+            self.viewer.set_colors(colors)
+        except Exception as e:
+            self.err_msg = traceback.format_exc()
+            self.err_msg += "\n" + str(e)
+
+    def _loadInBackground(self):
+        self.load()
+        self.emit(self.background_task_signal)
+
+    def load(self):
+        """
+        This can happen in a background thread,
+        so beware not to call any GUI methods.
+        """
+        try:
+            self.lasf_object = lasf.File(self.filename)
+        except Exception as e:
+            self.lasf_object = None
+            self.err_msg = str(e)
+            return
+        else:
+            # Transfer x, y, z (will reset position)
+            self.viewer.set_points(self.lasf_object.x,
+                                   self.lasf_object.y,
+                                   self.lasf_object.z)
+            self.setColors()
+
+    def log(self, text):
+        self.emit(self.log_stdout_signal, text)
+
+    def logDebug(self, text, color="blue"):
+        # For logging to logWindow (debug etc...)
+        # TODO: use event propagation to capture
+        #       logging from background threads...
+        self.logWindow.log(text, color)
+
+    @pyqtSlot(str)
+    def logStderr(self, text):
+        # Show traceback etc...
+        self.logWindow.show()
+        self.logWindow.log(text, "red")
+
+    @pyqtSlot(str)
+    def logStdout(self, text):
+        # Just for small temporary messages
+        self.statusBar().showMessage(text)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
+        fname = sys.argv[1]
+    else:
+        fname = None
+    app = QtGui.QApplication(sys.argv)
+    window = LasViewer(fname)
+    sys.exit(app.exec_())
---
+++ qt_glviewer.py
@@ -0,0 +1,320 @@
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtOpenGL import *
+import OpenGL.GL as gl
+import OpenGL.GLU as glu
+from OpenGL.arrays import vbo
+import math
+import numpy as np
+
+"""
+OpenGL pointcloud rendering.
+Minor modifications of original laspy code.
+Generalized - is not LAS specific.
+"""
+
+
+class VBOProvider(object):
+
+    def __init__(self, x, y, z, colors, center):
+        n = x.shape[0]
+        i0 = 0
+        vbsize = 2000000
+        self.vbos = []
+        while i0 < n:
+            i1 = min(n, i0 + vbsize)
+            data = np.column_stack((x[i0:i1] - center[0], y[i0:i1] - center[1], z[
+                                   i0:i1] - center[2], colors[i0:i1])).astype(np.float32)
+            vbo_ = vbo.VBO(data=data, usage=gl.GL_DYNAMIC_DRAW,
+                           target=gl.GL_ARRAY_BUFFER)
+            self.vbos.append((vbo_, i1 - i0))
+            i0 += vbsize
+
+    def draw(self):
+        for vbo_, n in self.vbos:
+            vbo_.bind()
+            gl.glVertexPointer(3, gl.GL_FLOAT, 24, vbo_)
+            gl.glColorPointer(3, gl.GL_FLOAT, 24, vbo_ + 12)
+            gl.glDrawArrays(gl.GL_POINTS, 0, n)
+            vbo_.unbind()
+
+
+class PointcloudViewerWidget(QGLWidget):
+    """
+    A simple QGLWidget which can display a pointcloud with some
+    colors. This object is completely oblivious to where the data comes
+    from - will just need x, y, z and some colors.
+    """
+
+    def __init__(self, parent):
+        QGLWidget.__init__(self, parent)
+        self.setMouseTracking(True)
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.parent = parent
+        self.initial_z = 1500
+        self.location = np.array([0.0, 0.0, self.initial_z])
+        self.focus = np.array([0.0, 0.0, 0.0])
+        self.up = np.array([0.0, 1.0, 0.0])
+        self.center = np.array([0.0, 0.0, 0.0])
+        self.dist = self.initial_z
+        self.real_pos = self.location + self.center
+        self.data_buffer = None
+        self.movement_granularity = 6.0
+        self.look_granularity = 16.0
+        self.setMinimumSize(500, 500)
+        self.old_mouse_x = 0
+        self.old_mouse_y = 0
+        self.mouse_speed = 0.4
+        self.point_size = 1
+        self.x = None
+        self.y = None
+        self.z = None
+        self.colors = None
+
+    def increase_point_size(self):
+        if self.point_size < 5:
+            self.point_size += 1
+            gl.glPointSize(self.point_size)
+            self.update()
+            self.setFocus()
+
+    def decrease_point_size(self):
+        if self.point_size > 1:
+            self.point_size -= 1
+            gl.glPointSize(self.point_size)
+            self.update()
+            self.setFocus()
+
+    def set_points(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.center[0] = x.mean()
+        self.center[1] = y.mean()
+        self.center[2] = z.mean()
+        xmin = x.min()
+        ymin = y.min()
+        r = max(self.center[0] - xmin, self.center[1] - ymin)
+        self.initial_z = r * 1.5
+        self.location = np.array([0.0, 0.0, self.initial_z])
+        self.movement_granularity = max(r / 500.0 * 6, 1)
+        self.real_pos = self.location + self.center
+
+    def set_colors(self, colors):
+        self.colors = colors
+
+    def update_view(self):
+
+        self.data_buffer = VBOProvider(self.x, self.y, self.z,
+                                       self.colors, self.center)
+        self.update()
+        self.setFocus()
+
+    def paintGL(self):
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glLoadIdentity()
+        glu.gluLookAt(self.location[0], self.location[1], self.location[2],
+                      self.focus[0], self.focus[1], self.focus[2],
+                      self.up[0], self.up[1], self.up[2])
+        if self.data_buffer is not None:
+            self.draw_points()
+            diff = self.focus - self.location
+            d = np.sqrt(diff.dot(diff))
+            self.renderText(10, 10, "Position: %.2f,%.2f,%.2f, dist: %.2f" % (
+                self.real_pos[0], self.real_pos[1], self.real_pos[2], d))
+
+    def resizeGL(self, w, h):
+        ratio = w if h == 0 else float(w) / h
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        gl.glViewport(0, 0, w, h)
+        gl.glLoadIdentity()
+        glu.gluPerspective(90, float(ratio), 0.001, 3000)
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+
+    def initializeGL(self):
+        gl.glClearColor(0.0, 0.0, 0.0, 1.0)
+        gl.glClearDepth(1.0)
+
+    def draw_points(self):
+        gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+        gl.glEnableClientState(gl.GL_COLOR_ARRAY)
+        self.data_buffer.draw()
+        gl.glDisableClientState(gl.GL_COLOR_ARRAY)
+        gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
+
+    def mouseMoveEvent(self, mouseEvent):
+        if int(mouseEvent.buttons()) != QtCore.Qt.NoButton:
+            # user is dragging
+            delta_x = mouseEvent.x() - self.old_mouse_x
+            delta_y = self.old_mouse_y - mouseEvent.y()
+            if int(mouseEvent.buttons()) & QtCore.Qt.LeftButton:
+                self.camera_yaw_pitch(delta_x * 0.03, delta_y * 0.03)
+                self.update()
+            elif int(mouseEvent.buttons()) & QtCore.Qt.RightButton:
+                self.camera_roll((delta_x) * 0.05)
+                self.update()
+        self.old_mouse_x = mouseEvent.x()
+        self.old_mouse_y = mouseEvent.y()
+
+    def rotate_vector(self, vec_rot, vec_about, theta):
+        d = np.sqrt(vec_about.dot(vec_about))
+
+        L = np.array((0, vec_about[2], -vec_about[1],
+                      -vec_about[2], 0, vec_about[0],
+                      vec_about[1], -vec_about[0], 0))
+        L.shape = (3, 3)
+
+        try:
+            R = (np.identity(3) + np.sin(theta) / d * L +
+                 (1 - np.cos(theta)) / (d * d) * (L.dot(L)))
+        except Exception:
+            print("Error in rotation.")
+            return()
+        return(vec_rot.dot(R))
+
+    def camera_reset(self):
+        self.location = np.array([0.0, 0.0, self.initial_z])
+        self.focus = np.array([0.0, 0.0, 0.0])
+        self.up = np.array([0.0, 1.0, 0.0])
+        self.real_pos = self.location + self.center
+        self.update()
+
+    def reset_all(self):
+        self.point_size = 1
+        gl.glPointSize(self.point_size)
+        self.camera_reset()
+        self.setFocus()  # will loose keyboard tracking else :-/
+
+    def camera_move(self, ammount, axis=1):
+
+        if axis == 1:
+            pointing = self.focus - self.location
+            pnorm = np.sqrt(pointing.dot(pointing))
+            pointing /= pnorm
+            self.location = self.location + ammount * pointing
+            self.focus = self.location + pnorm * pointing
+        elif axis == 2:
+            pointing = self.focus - self.location
+            direction = np.cross(self.up, pointing)
+            direction /= np.sqrt(direction.dot(direction))
+            self.location = self.location + ammount * direction
+            self.focus = self.location + pointing
+        elif axis == 3:
+            pointing = self.focus - self.location
+            direction = self.up
+            self.location = self.location + ammount * direction
+            self.focus = self.location + pointing
+        self.real_pos = self.location + self.center
+
+    def camera_yaw(self, theta):
+        pointing = self.focus - self.location
+        newpointing = self.rotate_vector(pointing, self.up, theta)
+        self.focus = newpointing + self.location
+
+    def camera_yaw_pitch(self, theta1, theta2):
+        pointing = self.focus - self.location
+        d1 = np.sqrt(pointing.dot(pointing))
+        newpointing1 = self.rotate_vector(pointing, self.up, theta1)
+        axis = np.cross(self.up, pointing)
+        newpointing2 = self.rotate_vector(pointing, axis, theta2)
+        self.up = np.cross(newpointing2, axis)
+        self.up /= np.sqrt(self.up.dot(self.up))
+        total = newpointing1 + newpointing2
+        d2 = np.sqrt(total.dot(total))
+        self.focus = total * d1 / d2 + self.location
+
+    def camera_roll(self, theta):
+        self.up = self.rotate_vector(
+            self.up, self.focus - self.location, theta)
+
+    def camera_pitch(self, theta):
+        pointing = self.focus - self.location
+        axis = np.cross(self.up, pointing)
+        newpointing = self.rotate_vector(pointing, axis, theta)
+        self.focus = newpointing + self.location
+        self.up = np.cross(newpointing, axis)
+        self.up /= np.sqrt(self.up.dot(self.up))
+
+    def wheelEvent(self, event):
+        if self.data_buffer is not None:
+            self.camera_move(event.delta() * self.movement_granularity * 0.03)
+            self.update()
+
+    def mouseDoubleClickEvent(self, event):
+        if self.data_buffer is not None:
+            self.camera_move(20)
+            self.update()
+
+    # for this to work - we seemingly need to give focus to this widget from
+    # time to time...
+    def keyPressEvent(self, event):
+        if self.data_buffer is not None:
+            if event.key() == QtCore.Qt.Key_A:
+                self.camera_move(self.movement_granularity, 2)
+                self.update()
+            elif event.key() == QtCore.Qt.Key_D:
+                self.camera_move(-self.movement_granularity, 2)
+                self.update()
+            elif event.key() == QtCore.Qt.Key_W:
+                self.camera_move(self.movement_granularity, 3)
+                self.update()
+            elif event.key() == QtCore.Qt.Key_S:
+                self.camera_move(-self.movement_granularity, 3)
+                self.update()
+            event.accept()
+        else:
+            event.ignore()
+
+
+class ViewerContainer(QtGui.QWidget):
+    """
+    A handy container widget for the PointcloudViewerWidget.
+    Not LAS specific. 
+    We can increase point size, decrease point size and
+    reset view
+    """
+
+    def __init__(self, parent):
+        QtGui.QWidget.__init__(self, parent)
+        # self.setupUi(self)
+        self.viewer = PointcloudViewerWidget(self)
+        v_layout = QVBoxLayout(self)
+        top_layout = QHBoxLayout()
+        top_layout.addStretch()
+        v_layout.addLayout(top_layout)
+        self.bt_ps_plus = QPushButton("+", self)
+        self.bt_ps_minus = QPushButton("-", self)
+        self.bt_reset_view = QPushButton("reset", self)
+        self.viewer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.controls = [self.bt_ps_plus, self.bt_ps_minus, self.bt_reset_view]
+        for bt, handler in zip(self.controls,
+                               ("on_ps_plus", "on_ps_minus", "on_reset")):
+            bt.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+            top_layout.addWidget(bt)
+            bt.clicked.connect(getattr(self, handler))
+        v_layout.addWidget(self.viewer)
+        self.set_loaded_state(False)
+
+    def set_loaded_state(self, is_loaded):
+        for widget in self.controls:
+            widget.setEnabled(is_loaded)
+
+    def on_reset(self):
+        self.viewer.reset_all()
+
+    def on_ps_plus(self):
+        self.viewer.increase_point_size()
+
+    def on_ps_minus(self):
+        self.viewer.decrease_point_size()
+
+    def set_points(self, x, y, z):
+        self.viewer.set_points(x, y, z)
+
+    def set_colors(self, colors):
+        self.viewer.set_colors(colors)
+
+    def update_view(self):
+        self.viewer.update_view()
